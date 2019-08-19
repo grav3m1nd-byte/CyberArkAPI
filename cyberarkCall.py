3ac67086386b2eddd8cf0f953f7c71207c65ffec
grav3m1nd-byte <54182057+grav3m1nd-byte@users.noreply.github.com>
2019-08-19 15:36:33 -0400

    Update cyberarkCall.py
    This python file is only to demo the CyberArkAPI class. More to come.

--- cyberarkCall.py
+++ cyberarkCall.py
@@ -1,40 +1,57 @@
-import sys, getopt
+import getopt
+import sys
 
-from cyberarkAPI import cyberarkAPI
+from cyberarkAPI import CyberarkAPI
+
 
 # def call(cyberarkAPI):
 
 
 def main(argv):
+    # host = appid = safe = obj = folder = ""
+    account1 = CyberarkAPI()
+
     try:
-        opts, args = getopt.getopt(argv, "hf:nHaso:", ["num=", "Host=", "APPID=", "Safe=", "Obj=", "folder=", "help"])
-        account1 = cyberarkAPI()
+        # opts, args = getopt.getopt(argv, "hf:nHaso:", ["num=", "Host=", "APPID=", "Safe=", "Obj=", "folder=", "help"])
+        opts, args = getopt.getopt(sys.argv[1:], "H:a:s:o:f:h", ["Host=", "APPID=", "Safe=", "Obj=", "folder=", "help"])
+
     except getopt.GetoptError:
         # print("Usage: cyberarkCall.py -n <account_numbers> -H <CyberArk_Host> -a <APPID> -s <safe_name> -o "
-            # "<object_name> -f <folder (optional)")
-        print("Usage: cyberarkCall.py -H <CyberArk_Host> -a <APPID> -s <safe_name> -o <object_name> -f <folder (optional)")
+        # "<object_name> -f <folder (optional)")
+        print("Usage: cyberarkCall.py -H <CyberArk_Host> -a <APPID> -s <safe_name> -o <object_name> -f <folder> (opt)")
         sys.exit(2)
     for opt, arg in opts:
         if opt in ('-h', "help", "--help"):
             # print("Usage: cyberarkCall.py -n <account_numbers> -H <CyberArk_Host> -a <APPID> -s <safe_name> -o "
             # "<object_name> -f <folder (optional)")
-            print("Usage: cyberarkCall.py -H <CyberArk_Host> -a <APPID> -s <safe_name> -o <object_name> -f <folder (optional)")
+            print(
+                "Usage: cyberarkCall.py -H <CyberArk_Host> -a <APPID> -s <safe_name> -o <object_name> -f <folder> (opt)")
             sys.exit()
-#        elif opt in ("-n", "--num"):
-#            inputfile = arg
+            # elif opt in ("-n", "--num"):
+            # inputfile = arg
         elif opt in ("-H", "--Host"):
             account1.setHost(arg)
         elif opt in ("-a", "--APPID"):
-            account1.setappID(arg)
+            account1.appID = arg
         elif opt in ("-s", "--Safe"):
-            account1.setSafe(arg)
+            account1.safe = arg
         elif opt in ("-o", "--Obj"):
-            account1.setActName(arg)
+            account1.actName = arg
         elif opt in ("-o", "--folder"):
-            account1.setFolder(arg)
+            account1.folder = arg
+    #
+    #     account1 = cyberarkAPI(appid, safe, obj, host)
 
-    print(account1.host, account1.aimURL, account1.appID, account1.safe, account1.actName)
-    # print(account1.host, account1.aimURL)
+    # account1.setappID("test_AIM")
+    # account1.setSafe("Priv_BerriosJN")
+    # account1.setActName("OS-WinDomain-US1-BerriosJN")
+    # account1.setHost("cyberark.autonation.com")
+    # account1.host = "cyberark.autonation.com"
+    # account1.setaimURL(account1.host)
+    # account1.setaimURL(account1.getHost())
+
+    # print(account1.getaimURL())
+    print(account1.host, account1.getaimURL(), account1.getquerystring())
     # print(account1.querystring)
     # print(account1.getaimResponse().status_code)
     # account1.getaimData()
