6ce9762b0da7318ab61646fe1a1ffad3eef2e5b7
Jorge Berrios <jxberrios@gmail.com>
2019-08-21 14:10:11 -0400

    CyberArk AIMWebService Package
    Signed-off-by: Jorge Berrios <jxberrios@gmail.com>

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='CyberArk AIMWebService Call',
+    version='0.1.5',
+    packages=['cyberarkAIM'],
+    url='https://github.com/grav3m1nd-byte/CyberArkAPI',
+    license='',
+    author='Jorge Berrios',
+    author_email='jxberrios@gmail.com',
+    description='Package to perform CyberArk AIMWebService Calls'
+)
